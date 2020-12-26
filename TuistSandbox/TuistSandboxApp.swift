469c204d9b7a32afa209478cb486251c3a2a45bb
Joe Blau <im@joeblau.com>
2020-12-26 09:23:38 -0800

    Initial Commit

---
+++ TuistSandbox/TuistSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuistSandboxApp.swift
+//  TuistSandbox
+//
+//  Created by Joe Blau on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct TuistSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
